f43c5a352417ca7879457ce41bd2be1cbd088559
Subhajit Sahu <wolfram77@gmail.com>
2023-01-05 23:11:27 +0530

    :bug: use double quotes for POS_LOOKUP

--- src/POS_LOOKUP.ts
+++ src/POS_LOOKUP.ts
@@ -1,8 +1,8 @@
 const POS_LOOKUP = new Map([
-  ['n', 1],
-  ['v', 2],
-  ['a', 3],
-  ['r', 4],
-  ['s', 5]
+  ["n", 1],
+  ["v", 2],
+  ["a", 3],
+  ["r", 4],
+  ["s", 5]
 ]);
 export default POS_LOOKUP;
