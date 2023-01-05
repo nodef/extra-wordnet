c4b39e1aea360554df7da53383c6d8eef74b1db8
Subhajit Sahu <wolfram77@gmail.com>
2023-01-05 23:33:47 +0530

    :bug: w/ POINTER*

---
+++ src/POS_NAME_LOOKUP.ts
@@ -0,0 +1,8 @@
+const POS_NAME_LOOKUP = new Map([
+  ['n', 'noun'],
+  ['v', 'verb'],
+  ['a', 'adjective'],
+  ['r', 'adverb'],
+  ['s', 'adjective satellite']
+]);
+export default POS_NAME_LOOKUP;
